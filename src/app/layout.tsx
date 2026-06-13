7cf07adfd419e07eea6bb9a276b63375fbc749d8
plml <pierre.lemmel@gmail.com>
2026-06-13 12:43:32 +0200

    opn graph nifo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,28 @@ import BurgerMenu from "@/components/burger-menu";
 export const metadata: Metadata = {
 	title: "Très chair corps",
 	description: "Très chair corps - seule en scène improvisé",
+	metadataBase: new URL("https://treschaircorps.fr"),
+	icons: {
+		icon: "/favicon.svg",
+	},
+	openGraph: {
+		title: "Très chair corps",
+		description: "Très chair corps - seule en scène improvisé",
+		images: [
+			{
+				url: "/og/tcc-cover.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Très chair corps",
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Très chair corps",
+		description: "Très chair corps - seule en scène improvisé",
+		images: ["/og/tcc-cover.jpg"],
+	},
 };
 
 
